1eec0a545210be9966f2bee8ec8ffdf37023cfc5
Nver Khachoyan <khachoyannver@gmail.com>
2026-01-12 18:36:53 -0800

    move supported tmux opts to nix

--- home/programs/tmux/default.nix
+++ home/programs/tmux/default.nix
@@ -3,8 +3,20 @@
 {
   programs.tmux = {
     enable = true;
+
+    prefix = "C-Space";
+    baseIndex = 1;
+    escapeTime = 0;
+    historyLimit = 50000;
+    mouse = true;
+    aggressiveResize = false;
+    terminal = "tmux-256color";
+    keyMode = "vi";
+
+    # sensibleOnTop runs sensible plugin before custom conf
+    sensibleOnTop = true;
+
     plugins = with pkgs; [
-      tmuxPlugins.sensible
       tmuxPlugins.dracula
     ];
 

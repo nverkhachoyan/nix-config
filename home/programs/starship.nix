6f877693565b5f7f7dc42a26d7eae1973f24b061
Nver Khachoyan <khachoyannver@gmail.com>
2026-01-12 20:52:18 -0800

    rm comments

--- home/programs/starship.nix
+++ home/programs/starship.nix
@@ -5,28 +5,23 @@
     enable = true;
     enableZshIntegration = true;
     settings = {
-      # Move the prompt to a single line for a tighter feel
       add_newline = false;
 
-      # The prompt layout: keep it simple
       format = "$directory$git_branch$git_status$character";
 
-      # Clean up the directory look
       directory = {
         style = "bold blue";
-        truncate_to_repo = true; # Focus on the project root
+        truncate_to_repo = true;
         truncation_length = 3;
         fish_style_pwd_dir_length = 1; # '~/w/p/my-project'
       };
 
-      # Subtle Character (the prompt symbol)
       character = {
         success_symbol = "[󰄾](bold green)";
         error_symbol = "[󰄾](bold red)";
         vicmd_symbol = "[󰄾](bold yellow)";
       };
 
-      # Minimal Git
       git_branch = {
         symbol = "󰊢 ";
         format = "on [$symbol$branch]($style) ";
@@ -38,7 +33,6 @@
         style = "red";
       };
 
-      # Disable things that add clutter
       package.disabled = true;
       python.disabled = true;
       nodejs.disabled = true;

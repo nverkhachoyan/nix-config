5f923fcb20f47c0ecf444e7362d1faedc8bc6e7a
Nver Khachoyan <khachoyannver@gmail.com>
2026-01-29 17:14:23 -0800

    add ssh auth sock to 1password

--- home/home.nix
+++ home/home.nix
@@ -18,6 +18,10 @@
   home.homeDirectory = "/Users/${username}";
   home.stateVersion = "24.05";
 
+  home.sessionVariables = {
+  SSH_AUTH_SOCK = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+};
+
   xdg.enable = true;
   programs.home-manager.enable = true;
 }

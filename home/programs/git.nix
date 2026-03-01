e944dd058f362109dabed43188cde550a74c2c04
Nver Khachoyan <khachoyannver@gmail.com>
2026-02-28 21:37:20 -0800

    nvm we don't need to do that :D

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -1,4 +1,9 @@
-{ lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   programs.git = {
@@ -16,10 +21,14 @@
       gpg.format = "ssh";
       commit.gpgsign = true;
       "gpg \"ssh\"".program = "${pkgs._1password-gui}/bin/op-ssh-sign";
+      "gpg \"ssh\"".allowedSignersFile = "${config.xdg.configHome}/git/allowed_signers";
 
       user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBoVotkT+jNCRAtiZM+tQSh/grcNL17yldLsy1OhnsSb";
     };
   };
+
+  home.file."${config.xdg.configHome}/git/allowed_signers".text =
+    "khachoyannver@gmail.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBoVotkT+jNCRAtiZM+tQSh/grcNL17yldLsy1OhnsSb\n";
 
   programs.lazygit = {
     enable = true;
--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -11,13 +11,6 @@
     dotDir = "${config.xdg.configHome}/zsh";
 
     initContent = ''
-      # Ensure 1Password SSH agent socket is available in interactive shells
-      if [ -S "$HOME/.1password/agent.sock" ]; then
-        export SSH_AUTH_SOCK="$HOME/.1password/agent.sock"
-      elif [ -S "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" ]; then
-        export SSH_AUTH_SOCK="$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-      fi
-
       # lt function (tree view with level parameter)
       lt() {
         eza --sort=type --tree --level "''${1:-2}"
